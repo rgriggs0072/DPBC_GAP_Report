765a1f1fd1123a165496bbf873ff133008dfe81d
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-05-03 20:50:14 -0700

    Update DPBC_GAP_Report.py

--- DPBC_GAP_Report.py
+++ DPBC_GAP_Report.py
@@ -113,9 +113,9 @@ def write_to_snowflake(df, warehouse, database, schema, table):
     df.fillna(value=np.nan, inplace=True)
 
 # Check if the STORE_NAME column contains empty values
-if df['STORE_NAME'].isnull().values.any():
+    if df['STORE_NAME'].isnull().values.any():
     st.warning("The STORE_NAME column contains empty values. Please fix the spreadsheet and upload again.")
-return
+    return
   
     # establish a new connection to Snowflake
     conn = snowflake.connector.connect(
