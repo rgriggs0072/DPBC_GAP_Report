2d3734738bd6b30f7f1c13ea03f9d342a53137ef
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-05-04 11:15:06 -0700

    Update DPBC_GAP_Report.py

--- DPBC_GAP_Report.py
+++ DPBC_GAP_Report.py
@@ -75,10 +75,10 @@ def format_sales_report(workbook):
     col_idx = None
     for i, col in enumerate(ws.iter_cols()):
         if col[0].value == "Buyer Count 2/1/2023 - 5/2/2023":
-        col_idx = i+1 # adjust for 0-indexing
+            col_idx = i+1 # adjust for 0-indexing
         break
 
-# Format column as number with no decimals
+    # Format column as number with no decimals
     if col_idx:
     for cell in ws.iter_cols(min_col=col_idx, max_col=col_idx):
         for c in cell:
