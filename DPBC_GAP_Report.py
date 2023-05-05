b92f888c1b9a316e65480c662216837458e742c1
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-05-05 15:39:56 -0700

    Update DPBC_GAP_Report.py

--- DPBC_GAP_Report.py
+++ DPBC_GAP_Report.py
@@ -259,18 +259,7 @@ def create_gap_report(conn):
 #    database='DATASETS',
 #    schema='DATASETS'
 #)
- # Load Snowflake credentials from the secrets.toml file
-    snowflake_creds = st.secrets["snowflake"]
 
-    # Establish a new connection to Snowflake
-    conn = snowflake.connector.connect(
-    account=snowflake_creds["account"],
-    user=snowflake_creds["user"],
-    password=snowflake_creds["password"],
-    warehouse=snowflake_creds["warehouse"],
-    database=snowflake_creds["database"],
-    schema=snowflake_creds["schema"]
- )
     
     
 if st.sidebar.button('Generate Gap Report :beers:'):
